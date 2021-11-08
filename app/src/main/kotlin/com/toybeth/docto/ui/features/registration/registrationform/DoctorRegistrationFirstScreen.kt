013c2aea6b27da8281db654a1ee5db298c21efe7
Atick Faisal <atickfaisal@gmail.com>
2021-11-09 05:28:33 +0600

    Refactor registration first screen with common ui elements

--- app/src/main/kotlin/com/toybeth/docto/ui/features/registration/registrationform/DoctorRegistrationFirstScreen.kt
+++ app/src/main/kotlin/com/toybeth/docto/ui/features/registration/registrationform/DoctorRegistrationFirstScreen.kt
@@ -7,12 +7,18 @@ import android.os.Build
 import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Help
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,19 +29,14 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.toybeth.docto.R
+import com.toybeth.docto.ui.common.components.DoktoButton
+import com.toybeth.docto.ui.common.components.DoktoTextFiled
 import com.toybeth.docto.ui.theme.DoktoAccent
-import com.toybeth.docto.ui.theme.DoktoSecondary
 
 @ExperimentalMaterialApi
-@ExperimentalUnitApi
 @Composable
 fun DoctorRegistrationFirstScreen(
     viewModel: RegistrationViewModel,
@@ -44,15 +45,9 @@ fun DoctorRegistrationFirstScreen(
 ) {
 
     val scrollState = rememberScrollState()
-
-    var expanded by remember { mutableStateOf(false) }
-    var imageUri by remember {
-        mutableStateOf<Uri?>(null)
-    }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
     val context = LocalContext.current
-    val bitmap = remember {
-        mutableStateOf<Bitmap?>(null)
-    }
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
@@ -60,18 +55,18 @@ fun DoctorRegistrationFirstScreen(
         imageUri = uri
         imageUri?.let {
             if (Build.VERSION.SDK_INT < 28) {
-                bitmap.value = MediaStore.Images
-                    .Media.getBitmap(context.contentResolver, it)
+                bitmap.value = MediaStore.Images.Media.getBitmap(
+                    context.contentResolver, it
+                )
 
             } else {
-                val source = ImageDecoder
-                    .createSource(context.contentResolver, it)
+                val source = ImageDecoder.createSource(context.contentResolver, it)
                 bitmap.value = ImageDecoder.decodeBitmap(source)
             }
         }
     }
 
-    return Column(
+    Column(
         modifier = Modifier
             .verticalScroll(
                 state = scrollState,
@@ -85,6 +80,9 @@ fun DoctorRegistrationFirstScreen(
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            // ------------------------- PROFILE IMAGE ----------------------- //
+
             if (bitmap.value != null) {
                 Image(
                     bitmap = bitmap.value!!.asImageBitmap(),
@@ -92,7 +90,7 @@ fun DoctorRegistrationFirstScreen(
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .size(140.dp)
-                        .clip(CircleShape)                       // clip to the circle shape
+                        .clip(CircleShape)
                         .border(2.dp, Color.Gray, CircleShape)
                 )
             } else {
@@ -106,84 +104,48 @@ fun DoctorRegistrationFirstScreen(
                         .border(2.dp, Color.White, CircleShape)
                 )
             }
-//            Column(
-//                horizontalAlignment = Alignment.CenterHorizontally
-//            ) {
+
             Spacer(modifier = Modifier.height(8.dp))
-            Button(
-                onClick = {
-                    launcher.launch("image/*")
-                },
-                shape = RoundedCornerShape(24.dp),
-                modifier = Modifier
-                    .height(36.dp)
-                    .width(140.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = DoktoSecondary
-                )
-            ) {
-                Text(text = "Choose", color = Color.White)
-            }
-//            }
-        }
-        Spacer(modifier = Modifier.height(30.dp))
-        Text(
-            stringResource(id = R.string.label_userid),
-            color = Color.White
-        )
-        Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.CenterEnd,
-        ) {
 
-            OutlinedTextField(
-                value = viewModel.userId.value,
-                onValueChange = {
-                    viewModel.userId.value = it
-                },
-                placeholder = {
-                    Text(stringResource(id = R.string.hint_userid))
-                },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(),
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.Black,
-                    disabledTextColor = Color.Gray,
-                    backgroundColor = Color.White,
-                    cursorColor = DoktoAccent,
-                    placeholderColor = Color.Gray,
-                    disabledPlaceholderColor = Color.Gray
-                ),
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-                shape = RoundedCornerShape(16.dp)
-            )
-            Button(
-                onClick = {
+            // ---------------------- CHOOSE IMAGE ---------------------- //
 
-                },
-                modifier = Modifier
-//                    .height(IntrinsicSize.Max)
-                    .width(120.dp)
-                    .padding(end = 8.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = DoktoSecondary
-                ),
-                shape = RoundedCornerShape(16.dp)
-            ) {
-                Text(
-                    text = stringResource(id = R.string.check_availability),
-                    color = Color.White,
-                    textAlign = TextAlign.Center,
-                    fontSize = 10.sp
-                )
+            DoktoButton(textResourceId = R.string.choose_photo) {
+                launcher.launch("image/*")
             }
         }
-        RegistrationFormTextField(
-            viewModel.name,
-            R.string.label_name,
-            R.string.hint_name
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // --------------------------- USER ID -------------------------- //
+        DoktoTextFiled(
+            textFieldValue = viewModel.userId.value,
+            hintResourceId = R.string.hint_userid,
+            labelResourceId = R.string.label_userid,
+            trailingIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.Help,
+                        contentDescription = stringResource(id = R.string.check_availability),
+                        tint = DoktoAccent
+                    )
+                }
+            },
+            errorMessage = null,
+            onValueChange = { viewModel.userId.value = it }
         )
         Spacer(modifier = Modifier.height(30.dp))
+
+        // --------------------------- NAME ----------------------- //
+        DoktoTextFiled(
+            textFieldValue = viewModel.name.value,
+            hintResourceId = R.string.hint_name,
+            labelResourceId = R.string.label_name,
+            errorMessage = null,
+            onValueChange = { viewModel.name.value = it }
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // -------------------- MOBILE NUMBER ------------------ //
+
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
@@ -196,77 +158,56 @@ fun DoctorRegistrationFirstScreen(
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            OutlinedTextField(
-                modifier = Modifier
-                    .width(120.dp)
-                    .clickable {
-                        showCountrySelectionDialog.invoke()
-                    },
-                readOnly = true,
-                enabled = false,
-                placeholder = {
-                    Text(stringResource(id = R.string.hint_country_code))
-                },
-                value = viewModel.getSelectedCountryCode(),
-                onValueChange = { },
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.Black,
-                    disabledTextColor = Color.Gray,
-                    backgroundColor = Color.White,
-                    cursorColor = DoktoAccent,
-                    placeholderColor = Color.Gray,
-                    disabledPlaceholderColor = Color.Gray
-                ),
-                shape = RoundedCornerShape(16.dp)
+            DoktoTextFiled(
+                modifier = Modifier.width(120.dp),
+                textFieldValue = viewModel.getSelectedCountryCode(),
+                hintResourceId = R.string.hint_country_code,
+                onClick = { showCountrySelectionDialog.invoke() },
+                onValueChange = { }
             )
-            TextField(
-                value = viewModel.mobileNumber.value,
-                onValueChange = {
-                    viewModel.mobileNumber.value = it
-                },
+
+            DoktoTextFiled(
                 modifier = Modifier
                     .weight(1f)
                     .padding(start = 20.dp),
-                placeholder = { Text(stringResource(id = R.string.hint_mobile_number)) },
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.Black,
-                    disabledTextColor = Color.Gray,
-                    backgroundColor = Color.White,
-                    cursorColor = DoktoAccent,
-                    placeholderColor = Color.Gray,
-                    disabledPlaceholderColor = Color.Gray
-                ),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Next
-                ),
-                shape = RoundedCornerShape(16.dp)
+                textFieldValue = viewModel.mobileNumber.value,
+                hintResourceId = R.string.hint_mobile_number,
+                onValueChange = { viewModel.mobileNumber.value = it }
             )
         }
-        RegistrationFormTextField(
-            viewModel.email,
-            R.string.label_email,
-            R.string.hint_email
-        )
-        RegistrationFormTextField(
-            viewModel.password,
-            R.string.label_password,
-            R.string.hint_password,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password
-            ),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        RegistrationFormTextField(
-            viewModel.confirmPassword,
-            R.string.label_confirm_password,
-            R.string.hint_confirm_password,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password
-            ),
-            visualTransformation = PasswordVisualTransformation()
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // -------------------------- EMAIL ------------------------ //
+
+        DoktoTextFiled(textFieldValue = viewModel.email.value,
+            hintResourceId = R.string.hint_email,
+            labelResourceId = R.string.label_email,
+            onValueChange = { viewModel.email.value = it }
         )
         Spacer(modifier = Modifier.height(30.dp))
+
+        // ------------------------ PASSWORD --------------------- //
+
+        DoktoTextFiled(textFieldValue = viewModel.password.value,
+            hintResourceId = R.string.hint_password,
+            labelResourceId = R.string.label_password,
+            visualTransformation = PasswordVisualTransformation(),
+            onValueChange = { viewModel.password.value = it }
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // --------------------- CONFIRM PASSWORD ---------------------- //
+
+        DoktoTextFiled(textFieldValue = viewModel.confirmPassword.value,
+            hintResourceId = R.string.hint_confirm_password,
+            labelResourceId = R.string.label_confirm_password,
+            visualTransformation = PasswordVisualTransformation(),
+            onValueChange = { viewModel.confirmPassword.value = it }
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // ---------------------------- GENDER ------------------------- //
+
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
@@ -284,31 +225,25 @@ fun DoctorRegistrationFirstScreen(
         ) {
             viewModel.gender.value = it
         }
-        RegistrationFormTextField(
-            viewModel.dateOfBirth,
-            R.string.label_date_of_birth,
-            R.string.hint_date_of_birth,
-            onClick = {
-                datePicker.invoke()
-            },
-            keyboardOptions = KeyboardOptions.Default
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // ----------------------------- DATE OF BIRTH --------------------- //
+
+        DoktoTextFiled(
+            textFieldValue = viewModel.dateOfBirth.value,
+            hintResourceId = R.string.hint_date_of_birth,
+            labelResourceId = R.string.label_date_of_birth,
+            onClick = { datePicker.invoke() },
+            onValueChange = { viewModel.dateOfBirth.value = it }
         )
-        Spacer(modifier = Modifier.height(50.dp))
-        Button(
-            onClick = {
-                viewModel.moveNext()
-            },
-            shape = RoundedCornerShape(24.dp),
-            modifier = Modifier
-                .height(56.dp)
-                .width(200.dp)
-                .align(Alignment.CenterHorizontally),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = DoktoSecondary
-            )
-        ) {
-            Text(text = stringResource(id = R.string.next), color = Color.White)
+        Spacer(modifier = Modifier.height(30.dp))
+
+        // ------------------------ NEXT BUTTON -------------------- //
+
+        DoktoButton(textResourceId = R.string.next) {
+            viewModel.moveNext()
         }
+
         Spacer(modifier = Modifier.height(50.dp))
     }
 }

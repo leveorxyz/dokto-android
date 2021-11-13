64c2e364386cb5c5399ecfb4a7e92a10904a24c9
Shafayat <shafayathossainkhan@gmail.com>
2021-11-13 18:43:25 +0600

    Patient registration API integration ongoign.

--- app/src/main/kotlin/com/toybeth/docto/data/ApiService.kt
+++ app/src/main/kotlin/com/toybeth/docto/data/ApiService.kt
@@ -1,9 +1,14 @@
 package com.toybeth.docto.data
 
+import com.toybeth.docto.data.registration.model.PatientRegistrationRequestBody
 import io.reactivex.Maybe
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
+    @POST("user/patient-signup/")
+    fun patientRegistration(@Body requestBody: PatientRegistrationRequestBody)
 }
---
+++ app/src/main/kotlin/com/toybeth/docto/data/registration/model/PatientRegistrationRequestBody.kt
@@ -0,0 +1,69 @@
+package com.toybeth.docto.data.registration.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PatientRegistrationRequestBody(
+
+	@SerializedName("referring_doctor_full_name")
+	val referringDoctorFullName: String? = null,
+
+	@SerializedName("insurance_policy_holder_name")
+	val insurancePolicyHolderName: String? = null,
+
+	@SerializedName("profile_photo")
+	val profilePhoto: String,
+
+	@SerializedName("gender")
+	val gender: String,
+
+	@SerializedName("identification_number")
+	val identificationNumber: String,
+
+	@SerializedName("city")
+	val city: String? = null,
+
+	@SerializedName("date_of_birth")
+	val dateOfBirth: String,
+
+	@SerializedName("referring_doctor_address")
+	val referringDoctorAddress: String? = null,
+
+	@SerializedName("identification_type")
+	val identificationType: String,
+
+	@SerializedName("insurance_number")
+	val insuranceNumber: String? = null,
+
+	@SerializedName("zip_code")
+	val zipCode: String,
+
+	@SerializedName("insurance_type")
+	val insuranceType: String,
+
+	@SerializedName("password")
+	val password: String,
+
+	@SerializedName("full_name")
+	val fullName: String,
+
+	@SerializedName("street")
+	val street: String,
+
+	@SerializedName("contact_no")
+	val contactNo: String,
+
+	@SerializedName("social_security_number")
+	val socialSecurityNumber: String,
+
+	@SerializedName("state")
+	val state: String,
+
+	@SerializedName("insurance_name")
+	val insuranceName: String? = null,
+
+	@SerializedName("referring_doctor_phone_number")
+	val referringDoctorPhoneNumber: String? = null,
+
+	@SerializedName("email")
+	val email: String
+)

b6ece409935a8b50d53ff7c90d67cbd911e7bd2c
Atick Faisal <atickfaisal@gmail.com>
2021-11-25 22:52:35 +0600

    Make checkbox text clickable

--- app/src/main/kotlin/com/toybeth/docto/ui/common/components/DoktoCheckBox.kt
+++ app/src/main/kotlin/com/toybeth/docto/ui/common/components/DoktoCheckBox.kt
@@ -1,12 +1,12 @@
 package com.toybeth.docto.ui.common.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Checkbox
-import androidx.compose.material.CheckboxColors
 import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,7 +14,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.toybeth.docto.base.theme.DoktoError
-import com.toybeth.docto.base.theme.DoktoPrimary
 import com.toybeth.docto.base.theme.DoktoPrimaryVariant
 
 @Composable
@@ -31,6 +30,7 @@ fun DoktoCheckBox(
             .fillMaxWidth()
             .then(modifier)
     ) {
+        var isChecked by remember { mutableStateOf(false) }
         // ---------------------- CHECK BOX ------------------- //
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -43,11 +43,18 @@ fun DoktoCheckBox(
                     uncheckedColor = Color.LightGray,
                     checkmarkColor = Color.White
                 ),
-                onCheckedChange = { onCheckedChange(it) }
+                onCheckedChange = {
+                    isChecked = it
+                    onCheckedChange(it)
+                }
             )
             Spacer(modifier = Modifier.width(8.dp))
             if (textResourceId != null) {
                 Text(
+                    modifier = Modifier.clickable {
+                        isChecked = !isChecked
+                        onCheckedChange(isChecked)
+                    },
                     text = stringResource(id = textResourceId),
                     color = Color.LightGray,
                     fontSize = 14.sp

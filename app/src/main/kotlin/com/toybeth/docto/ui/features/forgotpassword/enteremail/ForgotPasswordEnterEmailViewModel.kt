730f20bd6e17bb6f092b76d3df5f8e120226fcf0
Atick Faisal <atickfaisal@gmail.com>
2021-11-25 11:09:15 +0600

    Add email validation

--- app/src/main/kotlin/com/toybeth/docto/ui/features/forgotpassword/enteremail/ForgotPasswordEnterEmailViewModel.kt
+++ app/src/main/kotlin/com/toybeth/docto/ui/features/forgotpassword/enteremail/ForgotPasswordEnterEmailViewModel.kt
@@ -2,15 +2,37 @@ package com.toybeth.docto.ui.features.forgotpassword.enteremail
 
 import com.toybeth.docto.base.ui.BaseViewModel
 import com.toybeth.docto.base.utils.SingleLiveEvent
+import com.toybeth.docto.base.utils.extensions.isEmailValid
+import com.toybeth.docto.data.Property
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ForgotPasswordEnterEmailViewModel @Inject constructor() : BaseViewModel() {
 
+    val email = Property<String>()
     val navigateToOtp = SingleLiveEvent<Boolean>()
 
-    fun sendOtp(email: String) {
-        navigateToOtp.postValue(true)
+    private fun verifyEmail(): Boolean {
+        var isValid = true
+
+        if (email.state.value.isNullOrEmpty()) {
+            email.error.value = "This field is required"
+            isValid = false
+        }
+
+        if (!email.state.value.isEmailValid()) {
+            email.error.value = "Not valid email"
+            isValid = false
+        }
+
+        return isValid
+    }
+
+    fun sendOtp() {
+        if (verifyEmail()) {
+            // TODO: Send OTP to email
+            navigateToOtp.postValue(true)
+        }
     }
 }

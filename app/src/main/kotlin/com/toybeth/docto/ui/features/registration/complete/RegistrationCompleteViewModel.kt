a850ca25de71fb0875fca9f61b944165681be10a
Atick Faisal <atickfaisal@gmail.com>
2021-11-25 23:26:19 +0600

    Add registration complete fragment

---
+++ app/src/main/kotlin/com/toybeth/docto/ui/features/registration/complete/RegistrationCompleteViewModel.kt
@@ -0,0 +1,8 @@
+package com.toybeth.docto.ui.features.registration.complete
+
+import com.toybeth.docto.base.ui.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RegistrationCompleteViewModel @Inject constructor() : BaseViewModel()
